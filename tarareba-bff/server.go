c8707dc9ede0922a42f8c671e8b439fc55473240
monkukui <monkukui1953@gmail.com>
2020-11-21 18:35:34 +0900

    Add: BFF 側の cors の設定を追加

--- tarareba-bff/server.go
+++ tarareba-bff/server.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/rs/cors"
 )
 
 const defaultPort = "8080"
@@ -22,7 +23,13 @@ func main() {
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000/", "http://localhost:8080/"},
+		AllowCredentials: true,
+	})
+
+	http.Handle("/query", c.Handler(srv))
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
