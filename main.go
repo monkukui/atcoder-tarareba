a2f1ead2d0542e6e76e67aad41a84b906b31281c
monkukui <monkukui1953@gmail.com>
2020-12-30 23:33:58 +0900

    Add: host static html using echo

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/labstack/echo"
+)
+
+func main() {
+	e := echo.New()
+
+	e.File("/", "tarareba-frontend/build/index.html")
+	e.Static("/static/js", "tarareba-frontend/build/static/js")
+	e.Static("/static/css", "tarareba-frontend/build/static/css")
+	e.Static("/", "client/dist/favicon.ico")
+
+	e.Logger.Fatal(e.Start(":1213"))
+}
